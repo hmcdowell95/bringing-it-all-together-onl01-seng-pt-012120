18023b5d2eb78aa8f0b91de5ae37dd2c579619c1
Harley McDowell <hmcdowell95@gmail.com>
2020-04-01 02:26:34 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,7 +1,7 @@
 class Dog
   attr_accessor :id, :name, :breed 
   
-  def initialize (id:nil, name:, breed:)
+  def initialize (id: nil, name:, breed:)
     @id = id
     @name = name
     @breed = breed 
@@ -22,10 +22,7 @@ class Dog
   end
   
   def self.new_from_db(row)
-    new_dog = self.new
-    new_dog.id = row[0]
-    new_dog.name = row[1]
-    new_dog.breed = row[2]
+    new_dog = self.new(id: row[0], name: row[1], breed: row[2])
     new_dog
   end
   
